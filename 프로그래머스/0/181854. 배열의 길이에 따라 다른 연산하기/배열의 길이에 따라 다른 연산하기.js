2afe1cc0b9f64ddd5751b7e97f8b4fe2d502478e
류인태 <dlsxichlrg@naver.com>
2025-12-05 14:16:59 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,27 +1,17 @@
 function solution(arr, n) {
-    // arr 길이 짝수 홀수 구분
-    // 1. arr 길이가 홀수이면 arr의 짝수 인덱스 위치에 n 더하기
-    // 2. arr 길이가 짝수라면 arr의 홀수 인덱스 위치에 n 더하기
+    // 결과 배열을 원본 복사로 시작
+    let temp = [...arr];
 
-    // arr의 idx 가 짝수이거나 홀수일 때를 구분해서 n 값만 더하기
-
-    if (arr.length % 2 === 0) {
-        // 배열 길이가 짝수 일때 홀수 인덱스에만 n 값 더하기
-        for(let i = 0; i<arr.length; i++){
-            // 여기서 idx가 홀수에만 넣어야함
-            if (i% 2 ===1){
-                arr[i]+=n;
-            }
+    for (let i = 0; i < arr.length; i++) {
+        // 배열 길이가 짝수면 홀수 인덱스에 n 더하기
+        if (arr.length % 2 === 0 && i % 2 === 1) {
+            temp[i] = arr[i] + n;
         }
-    } else if (arr.length % 2 === 1){
-        for(let i = 0; i<arr.length; i++){
-            // 여기서 idx가 짝수에만 넣어야함
-            if (i% 2 ===0){
-                arr[i]+=n;
-            }
-
+        // 배열 길이가 홀수면 짝수 인덱스에 n 더하기
+        else if (arr.length % 2 === 1 && i % 2 === 0) {
+            temp[i] = arr[i] + n;
         }
     }
 
-        return arr;
+    return temp;
 }
