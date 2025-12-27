83c14501f95c9903214dcc5f7374287c3d13c836
류인태 <dlsxichlrg@naver.com>
2025-12-27 23:57:25 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,25 +1,11 @@
 function solution(numbers, direction) {
-
-    if(direction === "right"){
-    /*
-        1. 마지막 값을 꺼낸다
-        2. 그 값을 맨 앞에 넣는다
-     */
-        const last = numbers.pop()
-        numbers.unshift(last);
-        
-        return numbers;
-
-    }else if(direction === "left"){
-     /*
-        1. 맨 앞에있는 값을 꺼낸다
-        2. 그 값을 맨 뒤에 넣는다
-     */
-        const first = numbers.shift()
-        numbers.push(first);
-    
-        return numbers;
-
+    if (direction === "right") {
+        const last = numbers.slice(-1);
+        const rest = numbers.slice(0, -1);
+        return last.concat(rest);
+    } else {
+        const first = numbers.slice(0, 1);
+        const rest = numbers.slice(1);
+        return rest.concat(first);
     }
-
 }
