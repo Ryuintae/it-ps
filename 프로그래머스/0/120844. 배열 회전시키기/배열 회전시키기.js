e60ccde9fe95977b5ab4927a9f6d9eac00d57bd7
류인태 <dlsxichlrg@naver.com>
2025-12-28 00:22:34 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,11 +1,22 @@
 function solution(numbers, direction) {
-    if (direction === "right") {
-        const last = numbers.slice(-1);
-        const rest = numbers.slice(0, -1);
-        return last.concat(rest);
-    } else {
-        const first = numbers.slice(0, 1);
-        const rest = numbers.slice(1);
-        return rest.concat(first);
+    if(direction === 'right'){
+        // 오른쪽일땐 맨 뒤에 값 하나 추출
+        // 그 값을 -1을 제외한 앞에 값 확인
+        // concat 으로 lastvalue부터 뒤에 값을 concat 으로 추가
+        const lastValue = numbers.slice(-1);
+
+        // 1번째 배열부터 나머지 값 추출
+        const anotherValue = numbers.slice(0,-1);
+        return lastValue.concat(anotherValue);
+
+    }else if(direction === 'left'){
+        // 왼쪽일땐 맨 앞에 값을 추출
+        // 그 값을 맨 뒤에 추가
+        const firstValue = numbers.slice(0,1);
+
+        // 1번째 배열부터 나머지 값 추출
+        const anotherValue = numbers.slice(1);
+
+        return anotherValue.concat(firstValue);
     }
 }
